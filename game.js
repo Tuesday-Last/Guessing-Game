d95571f3451efa45e8d2fddd9d151c13373b4c70
Tuesday-Last <sgnortz@gmail>
2015-11-05 19:37:19 -0800

    first commit

---
+++ game.js
@@ -0,0 +1,48 @@
+var Bot = "Yes";
+var bot2 = "y";
+var bot3 = "yes";
+var Human = "No" ;
+var Human1 = "N" ;
+var Human2 = "no" ;
+var guess1 = prompt ("Do you find pop-ups annoying? Yes/No");
+	console.log(guess1);
+	if (guess1 == Human || guess1 == Human1 || guess1 == Human2) {
+	    alert("you win more pop-ups");
+	}
+	else if (guess1 == Bot || guess1 == bot2 || guess1 == bot3) {
+		alert("MORE POP-UPS! YAY!!");
+	}
+	else alert("you must answer Yes or No");
+var guess2 = prompt ("On a scale of 1-5 how annoying are these pop-ups?");
+	console.log(guess2);
+	alert(guess2);
+		if(guess2 == "1" || guess2 == "2") {
+			alert("Are you a robot?");
+	}
+		else if(guess2 == "3"){
+			alert("3, really?");
+	}
+		else alert("Man are you going to love this page");
+var guess3 = prompt ("was this pop-up as annoyingas the last one"?);
+console.log(guess3);
+	alert(guess3);
+		if(guess3 == "Yes" || guess3 == "yes" || guess3 == "y"){
+			alert("good, good you are still here though");
+		}
+		else alert("Ok");
+
+var guess5 = prompt('Has your annoyance reached a fever pitch yet?');
+console.log(guess5);
+	if(guess5 == "Yes" || guess5 == "yes" || guess5 == "y");
+		alert('HAHAHAHA IT IS WORKING!');
+		else alert("Well, well well. You certainly have a high tolerance for pop-ups")
+
+var guess4 = prompt("Do you you want to just see the page now? Yes/No");
+console.log(guess4);
+	alert(guess4);
+		if(guess4 == "Yes" || guess4 == "yes" || guess4 == "y"){
+			alert("HAHA there is nothing else to the page!");
+		}
+		else alert("Ok");
+
+		
